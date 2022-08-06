8914e21bdb7f2f8d88d4f950914539ad07b5ea61
Lassaad BAATI <lassaad.baati@gmail.com>
2022-08-06 17:05:41 +0100

    creation d un premier objet v1

--- Voiture.java
+++ Voiture.java
@@ -6,5 +6,14 @@ public class Voiture {
         x = 20;
         System.out.println(" test reussi");
         System.out.println(" test reussi avec X = "+x);
+
+        // Creer un objet Voiture BMW serie 3
+        Voiture v1 = new Voiture();
+        v1.serie = 3;
+        v1.marque = "BMW"; 
+        System.out.println("La voiture v1 serie : " + v1.serie);
+        System.out.println("La voiture v1 Marque : " + v1.marque);
     }
+
+
 }
