3dd18433c1a8f01df1eee2d7e4f25ecba8c6813a
Lassaad BAATI <lassaad.baati@gmail.com>
2022-08-06 16:37:42 +0100

    Add class Voiture.java

---
+++ Voiture.java
@@ -0,0 +1,10 @@
+public class Voiture {
+    public int serie;
+    public String marque; 
+    public static void main(String[] args){
+        int x ;
+        x = 20;
+        System.out.println(" test reussi");
+        System.out.println(" test reussi avec X = "+x);
+    }
+}
